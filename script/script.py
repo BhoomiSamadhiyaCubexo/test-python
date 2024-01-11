f4848824f8a0a2cae1e1245555ba6b0763d70e7b
BhoomiSamadhiyaCubexo <bhoomi.samadhiya@cubexo.io>
2024-01-11 19:23:56 +0530

    first sommit for dummy_api1

---
+++ script/script.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def response():
+      """
+      >>> response = requests.get(https://dummy.restapiexample.com/api/v1/employees)
+      print(response)
+
+      """
+
+
+
+if response:
+      print("success")
+else:
+      print("error")
+
+
+
+
+"-----------------------------------------------------------"
+
