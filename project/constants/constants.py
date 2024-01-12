ff1588f774dbbed19d396d77b817cedb867ef6b2
BhoomiSamadhiyaCubexo <bhoomi.samadhiya@cubexo.io>
2024-01-12 19:07:18 +0530

    changes pushed

---
+++ project/constants/constants.py
@@ -0,0 +1,11 @@
+#Configuraion constants:
+LOG_FILE = " "
+API_BASE_URL = "https://dummy.restapiexample.com/api/v1/employees"
+
+#default values
+DEFAULT_TIMEOUT = 0.001
+
+#status codes and messages
+STATUS_SUCCESS = 200
+STATUS_NOT_FOUND = 404
+ERROR_MESSAGE = 'An error occured'
