c2147a7756fe36b7b02b084bb21d52fc5d1017c6
Bhoomi <bhoomi.samadhiya@cubexo.io>
2024-01-23 11:38:27 +0530

    check

--- safeguard/fib1.py
+++
@@ -1,5 +0,0 @@
-a, b = 0, 1
-n = int(input("Enter any integer value: "))
-while a < n:
-    print(a)
-    a, b = b, a+b
--- utest2.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class MyTest(unittest.TestCase):
-    def test_addition(self):
-        result = 2 + 2
-        self.assertEqual(result, 5, msg="Adding 2 and 2 should equal 4, not 5")
-
-if __name__ == '__main__':
-    unittest.main()
